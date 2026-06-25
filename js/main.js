65761494bd7e848237f02ebe959de538dabcc6ef
Nariman <narimanhasanpanah@gmail.com>
2026-06-25 19:45:21 +0330

    Feat: add responsive superoffer section with countdown

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ setInterval(() => {
   const nextIndex = (slideIndex + 1) % slides.length;
   setSlide(nextIndex);
 }, 4000);
+
+// ----------countdown timer----------
+
+let remainingTime = 70000;
+const secondsInHour = 3600;
+const secondsInMinutes = 60;
+const hoursEl = document.querySelector("#hours");
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+
+function renderTime() {
+  if (remainingTime == 0) return;
+  let hours = Math.floor(remainingTime / secondsInHour);
+  let minutes = Math.floor((remainingTime % secondsInHour) / secondsInMinutes);
+  let seconds = (remainingTime % secondsInHour) % secondsInMinutes;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
+
+setInterval(() => {
+  remainingTime -= 1;
+  renderTime();
+}, 1000);
